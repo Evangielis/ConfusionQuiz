cd7f452989ed02669e1a2f3fdcccb65a4e7f3d6b
Evangielis <lee.painton@gmail.com>
2015-03-08 23:56:23 -0400

    Formulated structure for questions

---
+++ ConfusionQuiz/Quiz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfusionQuiz
+{
+    private class Question
+    {
+        string Text { get; set; }
+        List<string> Answers { get; set; }
+
+    }
+
+    class Quiz
+    {
+        List<Question> _questions;
+    }
+}
