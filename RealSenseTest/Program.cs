e7fe4cf9f2bdf938fa145e075e17ea4fb836df8a
Evangielis <lee.painton@gmail.com>
2015-03-20 12:56:59 -0400

    Added confusion quiz project

--- RealSenseTest/Program.cs
+++ RealSenseTest/Program.cs
@@ -10,34 +10,49 @@ namespace RealSenseTest
     {
         static void Main(string[] args)
         {
-// Create the SenseManager instance
-PXCMSenseManager sm=PXCMSenseManager.CreateInstance();
+            // Create the SenseManager instance
+            PXCMSession ss = PXCMSession.CreateInstance();
+            PXCMSenseManager sm = ss.CreateSenseManager();
  
-// Enable face trackcing
-sm.EnableFace();
+            // Enable face trackcing
+            sm.EnableFace();
  
-// Get a face instance here (or inside the AcquireFrame/ReleaseFrame loop) for configuration
-PXCMFaceModule face=sm.QueryFace();
-//...
+            // Get a face instance here (or inside the AcquireFrame/ReleaseFrame loop) for configuration
+            PXCMFaceModule face=sm.QueryFace();
+            //...
  
-// Initialize the pipeline
-sm.Init();
+            // Initialize the pipeline
+            sm.Init();
  
-// Stream data
-while (sm.AcquireFrame(true)>=pxcmStatus.PXCM_STATUS_NO_ERROR) {
-  // retrieve the face tracking results
-  PXCMFaceModule face2=sm.QueryFace();
-  if (sm!=null) {
-  //   ...
-      Console.WriteLine("Do stuff!");
-  }
+            // Stream data
+            while (sm.AcquireFrame(true)>=pxcmStatus.PXCM_STATUS_NO_ERROR) {
+              // retrieve the face tracking results
+              if (sm!=null) {
+                  PXCMFaceConfiguration conf = face.CreateActiveConfiguration();
+                  conf.EnableAllAlerts();
+                  conf.ApplyChanges();
+                  PXCMFaceData data = face.CreateOutput();
+                  PXCMFaceData.AlertData adata;
+                  try
+                  {
+                      Console.WriteLine(data.QueryNumberOfDetectedFaces());
+                      data.IsAlertFired(PXCMFaceData.AlertData.AlertType.ALERT_NEW_FACE_DETECTED, out adata);
+                      Console.WriteLine(adata.faceId);
+                  }
+                  finally
+                  {
+                      conf.Dispose();
+                      data.Dispose();
+                  }
+
+              }
  
-  // Resume next frame processing
-  sm.ReleaseFrame();
-}
+              // Resume next frame processing
+              sm.ReleaseFrame();
+            }
  
-// Clean up
-sm.Dispose();
+            // Clean up
+            sm.Dispose();
         }
     }
 }
